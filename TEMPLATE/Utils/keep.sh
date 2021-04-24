b4684bc3e4439603394069de40ba9c39a79c3329
Divyansh Rastogi <divyansh19464@iiitd.ac.in>
2021-04-24 14:34:33 +0530

    Create keep.sh

---
+++ TEMPLATE/Utils/keep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+
+echo "apt autoremove"
+apt autoremove
+
+echo "Cleaning apt cache"
+apt-get clean
+
+echo "Journal logs"
+journalctl --disk-usage
+journalctl --vacuum-size=100M
+
+# Todo for space
+# 1. Clear journal
+# 2. Vscode Reset intellisense database
+# 3. Vscode Clear Editor History
+# 4. Default stack size: ulimit -s 8192 (Current 8192 << 1)
