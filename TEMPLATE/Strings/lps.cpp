47a688789790f2d7349a0563e1f2ba544850f8b1
watch24hrs-iiitd <divyansh19464@iiitd.ac.in>
2021-08-16 20:02:21 +0530

    add lps

---
+++ TEMPLATE/Strings/lps.cpp
@@ -0,0 +1,79 @@
+# include "bits/stdc++.h"
+ 
+using namespace std;
+ 
+#define endl "\n"
+typedef long long int ll;
+#define f first
+#define s second
+ 
+#define SEND_HELP ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+ 
+// DEBUG
+void __print(int x) { cerr << x; }
+void __print(ll x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char* x) { cerr << '\"' << x << '\"'; }
+void __print(const string& x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+ 
+template<typename T, typename V>
+void __print(const pair<T, V>& x) { cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}'; }
+template<typename T>
+void __print(const T& x) { int _ = 0; cerr << '{'; for (auto& i : x) cerr << (_++ ? "," : ""), __print(i); cerr << "}"; }
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v) { __print(t); if (sizeof...(v)) cerr << ", "; _print(v...); }
+#ifdef LOCAL_PROJECT
+    #define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+    #define dbg(x...)
+#endif
+
+vector<int> getLPS(const string &s) {
+    int n = s.size();
+    int len = 0;
+    vector<int> lps(n);
+    int ptr = 1;
+    while (ptr < n) {
+        if (s[ptr] == s[len]) {
+            lps[ptr ++] = ++ len;
+        } else {
+            if (len) len = lps[len - 1];
+            else lps[ptr ++] = 0;
+        }
+    }
+    return lps;
+}
+
+void solve_tc(int test_case) {
+    
+}
+
+int main() { 
+    #ifdef LOCAL_PROJECT
+        freopen("inputf.in", "r", stdin); // LINUX
+    #endif
+    SEND_HELP
+
+    auto start_exec = chrono::high_resolution_clock::now();
+    int tt = 1;
+    cin >> tt; // TEST CASE INPUT
+    
+    for (int tc = 1; tc <= tt; ++ tc) {
+        solve_tc(tc);
+    }
+
+    auto stop_exec = chrono::high_resolution_clock::now();
+    auto duration_exec = chrono::duration_cast<chrono::milliseconds>(stop_exec - start_exec);
+    #ifdef LOCAL_PROJECT
+        cout << "\n\nDURATION EXECUTION: " << duration_exec.count() << "ms\n";
+    #endif
+
+    return 0;
+}
+
+/*
+https://github.com/watch24hrs-iiitd/CodingLib
+*/
